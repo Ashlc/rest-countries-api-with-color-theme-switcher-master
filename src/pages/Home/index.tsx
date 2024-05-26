e284202ce1c93e90b5ab125e6111d043ecdb547c
Ashlc <isadorabpaz@gmail.com>
2024-05-25 23:51:03 -0300

    :iphone: Improving responsiveness

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -80,8 +80,8 @@ const Index = () => {
     ];
 
     return (
-        <div className="flex flex-col py-10 px-10 lg:px-40 lg:py-20 w-full gap-10">
-            <div className="flex flex-col lg:flex-row lg:justify-between gap-4">
+        <div className="flex flex-col py-10 md:px-20 lg:px-40 lg:py-20 w-full gap-10">
+            <div className="flex flex-col px-4 md:px-0 lg:flex-row lg:justify-between gap-4">
                 <SearchBar value={value} setValue={setValue} />
                 <Filter
                     value={region}
